b58ef572bd040c111c03c2a83341bf2380ada6c4
Michael Raguse <michael.raguse@vipion.de>
2024-08-02 11:23:09 +0200

    [GYMX-1014] add theme provider for docs

--- .storybook/docs-theme-provider.tsx
+++ .storybook/docs-theme-provider.tsx
@@ -1,15 +1,26 @@
-import { DocsContainer, type DocsContextProps } from '@storybook/blocks'
-import { themes } from '@storybook/theming'
-import { type PropsWithChildren } from 'react'
-import * as React from 'react'
+import { type DocsContextProps, DocsContainer, Story } from '@storybook/blocks';
+import { themes } from '@storybook/theming';
+import { type PropsWithChildren } from 'react';
+import * as React from 'react';
 
 export function ThemedContainer(props: PropsWithChildren<{ context: DocsContextProps }>) {
+  /**
+   * Decorator function for theming the Docs Pages.
+   * The '@storybook/addon-themes' addon only updates the Story Pages.
+   * When switching to the Doc Pages, the data-theme attribute is only sporadically updated.
+   * Therefore, the manual update here in the Theme Provider
+   */
+  const theme = (props.context as any).store.globals.globals.theme;
+  document.documentElement.setAttribute('data-theme', theme);
+
   return (
     <DocsContainer
       context={props.context}
-      theme={(props.context as any).store.globals.globals.theme === 'dark' ? themes.dark : themes.light}
+      // key={(props.context as any).store.globals.globals.theme === 'dark' ? themes.dark.base : themes.light.base}
+      theme={theme === 'dark' ? themes.dark : themes.light}
     >
-      {props.children}
+      <div data-docs={'wtf'} data-theme={theme ?? 'wtf'}>{props.children}</div>
     </DocsContainer>
   )
 }
+/* vielleicht auf dom ebene prüfen ob data-theme schon existiert */
