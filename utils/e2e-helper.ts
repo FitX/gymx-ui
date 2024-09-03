3e8ae9baa50de23844f1af9375b520f305c12ba4
Michael Raguse <michael.raguse@vipion.de>
2024-09-03 11:36:39 +0200

    [GYMX-1016] fix e2e path

--- utils/e2e-helper.ts
+++ utils/e2e-helper.ts
@@ -1,1 +1,1 @@
-export const getUrlByStorybookId = (id: string) => `http://localhost:6007/iframe.html?viewMode=story&id=components-${id}--default&globals=theme:dark&args=`;
+export const getUrlByStorybookId = (id: string) => `http://localhost:6006/iframe.html?viewMode=story&id=components-${id}--default&globals=theme:dark&args=`;
