988b5452e597b704979da7ac480d534e245a58f2
Michael Raguse <michael.raguse@vipion.de>
2024-10-28 17:09:37 +0100

    [GYMX-842] docs

--- src/components/gymx-dialog/types.ts
+++ src/components/gymx-dialog/types.ts
@@ -1,6 +1,10 @@
 
 export interface GymxDialogProps {
   title?: string;
+  /**
+   * Whether the dialog should be modal or not.
+   * @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement/showModal
+   */
   isModal?: boolean;
   modelValue?: boolean;
   closeOnOutside?: boolean;
