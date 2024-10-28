036b132dd250db2ea54784bb321db5e53607ceaf
Michael Raguse <michael.raguse@vipion.de>
2024-10-28 10:46:10 +0100

    [GYMX-842] Dialog

--- src/components/gymx-dialog/gymx-dialog.vue
+++ src/components/gymx-dialog/gymx-dialog.vue
@@ -1,77 +1,109 @@
 
 <script lang="ts" setup>
-import { computed, ref } from 'vue';
+import { computed, onMounted, ref, watch } from 'vue';
 import { onClickOutside } from '@vueuse/core';
 import type { GymxDialogProps, GymxDialogSlots } from './types';
 
 const props = defineProps<GymxDialogProps>();
+const emit = defineEmits(['update:modelValue', 'opened', 'closed']);
 defineSlots<GymxDialogSlots>();
 
 const dialogEl = ref<HTMLDialogElement>();
 
-const isOpen = ref(false);
+// const isOpen = defineModel<boolean>();
 
 const open = () => {
-  if (true || props.isModal) {
+  if (props.isModal) {
     dialogEl.value?.showModal();
   } else {
     dialogEl.value?.show();
   }
+  emit('opened')
 };
 
 const handleClickOutside = (e) => {
-  console.log('ayay', e)
-  if (true || props.closeOnOutside) {
+  // console.log('ayay', e)
+  if (props.closeOnOutside) {
     close();
   }
 };
 
-onClickOutside(dialogEl, handleClickOutside);
+// onClickOutside(dialogEl, handleClickOutside);
 
 const close = () => {
   dialogEl.value?.close();
+  // isOpen.value = false;
+  emit('update:modelValue', false)
+  emit('closed');
 };
 
-const hallo = computed(() => dialogEl.value?.open)
+onMounted(() => {
+  watch(() => props.modelValue, (val) => {
+    console.log('watch modelValue', val)
+    if (val) {
+      open();
+    } else {
+      close();
+    }
+    console.log('el', dialogEl.value.open)
+    }, { immediate: true });
+});
+
+/*
+watch((isOpen), (val) => {
+  console.log('isOpen', val)
+  if (val) {
+    open();
+  } else {
+    close();
+  }});
+*/
+
 </script>
 <template>
   <div>
     <div class="pseudo-dialog-backdrop">
-      <dialog
-        ref="dialogEl"
-        class="dialog">
-        <div class="dialog__content">
-          <header class="dialog__header">
-            <slot name="header">
-              <h2 class="dialog__title">{{ props.title }}</h2>
-              <button class="dialog__button-close">Close</button>
-            </slot>
-          </header>
-          <div class="dialog__body">
-            <slot name="content" />
-          </div>
-          <div class="dialog__footer">
-            <slot name="footer" />
-          </div>
-        </div>
-      </dialog>
+
     </div>
-    <p>Dialog open {{ hallo }}</p>
-    <button @click="open()">open</button>
+
+    <dialog
+      ref="dialogEl"
+      class="dialog">
+      <div class="dialog__content">
+        <header class="dialog__header">
+          <slot name="header">
+            <h2 class="dialog__title">{{ props.title }}</h2>
+            <p>Lorem</p>
+            <button class="dialog__button-close" @click="close()">Close</button>
+          </slot>
+        </header>
+        <div class="dialog__body">
+          <slot name="content" />
+        </div>
+        <div class="dialog__footer">
+          <slot name="footer" />
+        </div>
+      </div>
+    </dialog>
+<!--
+    <p>Dialog open {{ props.modelValue }}</p>
+    <button @click="isOpen = !isOpen">open</button>
+    <button @click="open()">open</button>-->
   </div>
 </template>
 <style lang="scss" scoped>
 .pseudo-dialog-backdrop:has(dialog[open]) {
-  inset: 0;
+  /* inset: 0;
   background: red;
   position: fixed;
   pointer-events: none;
-  opacity: 0.2;
-  display: contents;
+  opacity: 0.2; */
+  // display: contents;
 }
 .dialog {
   padding: 4rem;
   box-shadow: 2px 2px 8px 0 rgba(0, 0, 0, 0.1);
+  z-index: 50;
 }
 .dialog::backdrop {
   // pointer-events: none;
