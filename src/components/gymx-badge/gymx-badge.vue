86b742b8caf6bc9d26512d70133aa6d6234ac322
Michael Raguse <michael.raguse@vipion.de>
2024-11-25 10:29:07 +0100

    [internal #7] Update Badges

--- src/components/gymx-badge/gymx-badge.vue
+++ src/components/gymx-badge/gymx-badge.vue
@@ -13,23 +13,25 @@ const props = defineProps<GymxBadgeProps>();
 <style lang="scss">
 </style>
 <style lang="scss" scoped>
-:where(.badge) {
-  --_badge-font-size: var(--gymx-badge-font-size, var(--gymx-font-size-1));
-  --_badge-color-background: var(--gymx-badge-color-background, var(--gymx-color-gray-2));
-  --_badge-color-text: var(--gymx-badge-color-text, var(--gymx-color-gray-12));
-  --_badge-radius: var(--gymx-badge-radius, var(--radius-2));
-  --_badge-padding-inline: var(--gymx-badge-padding-inline, var(--gymx-size-0));
-  --_badge-padding-block: var(--gymx-badge-padding-block, var(--gymx-size-000));
+.badge {
+  :where(&) {
+    --_gymx-badge-font-size: var(--gymx-badge-font-size, var(--gymx-font-size-1));
+    --_gymx-badge-color-background: var(--gymx-badge-color-background, var(--gymx-color-gray-2));
+    --_gymx-badge-color-text: var(--gymx-badge-color-text, var(--gymx-color-gray-12));
+    --_gymx-badge-radius: var(--gymx-badge-radius, var(--radius-2));
+    --_gymx-badge-padding-inline: var(--gymx-badge-padding-inline, var(--gymx-size-0));
+    --_gymx-badge-padding-block: var(--gymx-badge-padding-block, var(--gymx-size-000));
+  }
 
   display: inline-flex;
   place-items: center;
   place-content: center;
-  font-size: var(--_badge-font-size);
+  font-size: var(--_gymx-badge-font-size);
   font-weight: 400;
-  color: var(--_badge-color-text);
-  padding-inline: var(--_badge-padding-inline);
-  padding-block: var(--_badge-padding-block);
-  background: var(--_badge-color-background);
-  border-radius: var(--_badge-radius);
+  color: var(--_gymx-badge-color-text);
+  padding-inline: var(--_gymx-badge-padding-inline);
+  padding-block: var(--_gymx-badge-padding-block);
+  background: var(--_gymx-badge-color-background);
+  border-radius: var(--_gymx-badge-radius);
 }
 </style>
