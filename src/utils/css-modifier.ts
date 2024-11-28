2efaad709731b00424f5dc62534897a27d5237ec
Michael Raguse <michael.raguse@vipion.de>
2024-11-28 14:12:29 +0100

    [GYMX-1018] remove empty items from list

--- src/utils/css-modifier.ts
+++ src/utils/css-modifier.ts
@@ -1,10 +1,10 @@
 const getModifierClass = (block: string, modifier: string) => `${block}--${modifier}`;
-export const getModifierClasses = (block: string, modifiers?: string | string[]) => {
+export const getModifierClasses = (block: string, modifiers?: string | (string | undefined)[]) => {
   if (!modifiers) {
     return [];
   }
   if (typeof modifiers === 'string') {
     return [getModifierClass(block, modifiers)];
   }
-  return modifiers.map((mod) => getModifierClass(block, mod));
+  return modifiers.filter((mod) => mod).map((mod) => getModifierClass(block, mod));
 };
