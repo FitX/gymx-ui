f0ff179908dd333b992accbb0093ef983ce5a44a
Michael Raguse <michael.raguse@vipion.de>
2024-11-28 14:13:25 +0100

    [GYMX-1018] checkmark icon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,4 +1,5 @@
 import FancyIcon from '@/assets/icons/fancy-icon.svg'; // @deprecated
 import IconError from '@/assets/icons/error.svg';
 import IconClose from '@/assets/icons/close.svg';
-export { FancyIcon, IconError, IconClose };
+import IconCheckmark from '@/assets/icons/checkmark.svg';
+export { FancyIcon, IconError, IconClose, IconCheckmark };
