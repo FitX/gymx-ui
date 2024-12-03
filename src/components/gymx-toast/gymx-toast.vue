ddb4ee8eb33448307a30161e8feff0c8cb805e6c
Michael Raguse <michael.raguse@vipion.de>
2024-12-03 18:52:48 +0100

    [#7 internal] restructure custom properties
    - normalize toast props

--- src/components/gymx-toast/gymx-toast.vue
+++ src/components/gymx-toast/gymx-toast.vue
@@ -51,65 +51,68 @@ onMounted(() => {
   </div>
 </template>
 <style lang="scss" scoped>
+@use "@/assets/styles/component-utils/components.scss";
+
 .toast {
   $self: &;
-  --_toast-color-background: var(--toast-color-background, var(--gymx-color-gray-2));
-  --_toast-color: var(--toast-color, var(--gymx-color-gray-12));
-  --_toast-color-border: var(--toast-color-border, var(--toast-color-background));
-  --_toast-padding-inline: var(--toast-padding-inline, var(--gymx-size-0));
-  --_toast-padding-block: var(--toast-padding-block, var(--gymx-size-00));
-  --_toast-radius: var(--toast-radius, var(--gymx-radius-0));
-  --_toast-icon-fill: var(--toast-icon-fill, currentColor);
-  --_toast-icon-gap: var(--toast-icon-gap, var(--gymx-size-0));
+  // @TODO @include components.define-component-variable('gymx-toggle-switch', 'color-background', transparent);
+  --_gymx-toast-color-background: var(--gymx-toast-color-background, var(--gymx-color-gray-2));
+  --_gymx-toast-color: var(--gymx-toast-color, var(--gymx-color-gray-12));
+  --_gymx-toast-color-border: var(--gymx-toast-color-border, var(--gymx-toast-color-background));
+  --_gymx-toast-padding-inline: var(--gymx-toast-padding-inline, var(--gymx-size-0));
+  --_gymx-toast-padding-block: var(--gymx-toast-padding-block, var(--gymx-size-00));
+  --_gymx-toast-radius: var(--gymx-toast-radius, var(--gymx-radius-0));
+  --_gymx-toast-icon-fill: var(--gymx-toast-icon-fill, currentColor);
+  --_gymx-toast-icon-gap: var(--gymx-toast-icon-gap, var(--gymx-size-0));
 
-  --_toast-color-success: var(--toast-color-success, var(--gymx-color-black-12));
-  --_toast-color-background-success: var(
-    --toast-color-background-success,
+  --_gymx-toast-color-success: var(--gymx-toast-color-success, var(--gymx-color-black-12));
+  --_gymx-toast-color-background-success: var(
+    --gymx-toast-color-background-success,
     var(--gymx-color-success-9)
   );
 
-  --_toast-color-warning: var(--toast-color-warning, var(--gymx-color-black-12));
-  --_toast-color-background-warning: var(
-    --toast-color-background-warning,
+  --_gymx-toast-color-warning: var(--gymx-toast-color-warning, var(--gymx-color-black-12));
+  --_gymx-toast-color-background-warning: var(
+    --gymx-toast-color-background-warning,
     var(--gymx-color-warning-9)
   );
 
-  --_toast-color-error: var(--toast-color-error, var(--gymx-color-white-12));
-  --_toast-color-background-error: var(--toast-color-background-error, var(--gymx-color-error-9));
+  --_gymx-toast-color-error: var(--gymx-toast-color-error, var(--gymx-color-white-12));
+  --_gymx-toast-color-background-error: var(--gymx-toast-color-background-error, var(--gymx-color-error-9));
 
-  background: var(--_toast-color-background);
-  color: var(--_toast-color);
-  border: var(--_toast-color-border);
-  padding-inline: var(--_toast-padding-inline);
-  padding-block: var(--_toast-padding-block);
-  border-radius: var(--_toast-radius);
+  background: var(--_gymx-toast-color-background);
+  color: var(--_gymx-toast-color);
+  border: var(--_gymx-toast-color-border);
+  padding-inline: var(--_gymx-toast-padding-inline);
+  padding-block: var(--_gymx-toast-padding-block);
+  border-radius: var(--_gymx-toast-radius);
   display: inline-flex;
-  gap: var(--_toast-icon-gap);
+  gap: var(--_gymx-toast-icon-gap);
   align-items: center;
   align-content: center;
   justify-content: space-between;
 
   &--success {
-    --toast-color: var(--_toast-color-success);
-    --toast-color-background: var(--_toast-color-background-success);
+    --gymx-toast-color: var(--_gymx-toast-color-success);
+    --gymx-toast-color-background: var(--_gymx-toast-color-background-success);
   }
 
   &--warning {
-    --toast-color: var(--_toast-color-warning);
-    --toast-color-background: var(--_toast-color-background-warning);
+    --gymx-toast-color: var(--_gymx-toast-color-warning);
+    --gymx-toast-color-background: var(--_gymx-toast-color-background-warning);
   }
 
   &--error {
-    --toast-color: var(--_toast-color-error);
-    --toast-color-background: var(--_toast-color-background-error);
+    --gymx-toast-color: var(--_gymx-toast-color-error);
+    --gymx-toast-color-background: var(--_gymx-toast-color-background-error);
   }
 
   &__icon {
-    --icon-fill: var(--_toast-icon-fill);
+    --icon-fill: var(--_gymx-toast-icon-fill);
   }
 
   :deep(#{$self}__icon) {
-    --icon-fill: var(--_toast-icon-fill);
+    --icon-fill: var(--_gymx-toast-icon-fill);
   }
 
   &__btn-close {
