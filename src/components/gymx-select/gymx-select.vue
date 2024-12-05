9de0aefd42f946a8d4af1f4340a6efbfc86d0377
Michael Raguse <michael.raguse@vipion.de>
2024-12-05 19:48:12 +0100

    [#7 internal] normalize custom properties from select

--- src/components/gymx-select/gymx-select.vue
+++ src/components/gymx-select/gymx-select.vue
@@ -55,60 +55,60 @@ const [modelValue] = defineModel<SelectItem | SelectItem[]>();
 .select {
   $self: &;
 
-  --_select-font-family: var(--select-font-family, var(--gymx-font-family-default));
-  --_select-font-size: var(--select-font-size, var(--gymx-font-size-2));
-  --_select-font-weight: var(--select-font-letter-spacing, inherit);
+  --_gymx-select-font-family: var(--gymx-select-font-family, var(--gymx-font-family-default));
+  --_gymx-select-font-size: var(--gymx-select-font-size, var(--gymx-font-size-2));
+  --_gymx-select-font-weight: var(--gymx-select-font-letter-spacing, inherit);
 
   /* State defaults */
-  --_select-color: var(--select-color, var(--gymx-color-text));
-  --_select-color-background: var(--select-color-background, transparent);
-  --_select-color-border: var(--select-color-border, currentColor);
+  --_gymx-select-color: var(--gymx-select-color, var(--gymx-color-text));
+  --_gymx-select-color-background: var(--gymx-select-color-background, transparent);
+  --_gymx-select-color-border: var(--gymx-select-color-border, currentColor);
 
-  --_select-color-hover: var(--select-color-hover, inherit);
-  --_select-color-background-hover: var(--select-color-background-hover, initial);
-  --_select-color-border-hover: var(--select-color-border-hover, initial);
+  --_gymx-select-color-hover: var(--gymx-select-color-hover, inherit);
+  --_gymx-select-color-background-hover: var(--gymx-select-color-background-hover, initial);
+  --_gymx-select-color-border-hover: var(--gymx-select-color-border-hover, initial);
 
-  --_select-color-disabled: var(--select-color-disabled, var(--gymx-color-text));
-  --_select-color-background-disabled: var(
-    --select-color-background-disabled,
+  --_gymx-select-color-disabled: var(--gymx-select-color-disabled, var(--gymx-color-text));
+  --_gymx-select-color-background-disabled: var(
+    --gymx-select-color-background-disabled,
     var(--gymx-color-black-1)
   );
-  --_select-color-border-disabled: var(--select-color-border-disabled, var(--gymx-color-black-2));
+  --_gymx-select-color-border-disabled: var(--gymx-select-color-border-disabled, var(--gymx-color-black-2));
 
   /* Other */
-  --_select-radius: var(--select-radius, 0);
-  --_select-border: var(
-    --select-border,
-    var(--gymx-border-size-1, 1px) solid var(--_select-color-border, currentColor)
+  --_gymx-select-radius: var(--gymx-select-radius, 0);
+  --_gymx-select-border: var(
+    --gymx-select-border,
+    var(--gymx-border-size-1, 1px) solid var(--_gymx-select-color-border, currentColor)
   );
-  --_select-outline: var(--select-outline, var(--_select-border)); // maybe not
+  --_gymx-select-outline: var(--gymx-select-outline, var(--_gymx-select-border)); // maybe not
 
-  --_select-padding-inline: var(--select-padding-inline, var(--gymx-size-0));
-  --_select-padding-block: var(--select-padding-block, var(--gymx-size-00));
-  --_select-gap: var(--select-gap, var(--_select-padding-inline));
+  --_gymx-select-padding-inline: var(--gymx-select-padding-inline, var(--gymx-size-0));
+  --_gymx-select-padding-block: var(--gymx-select-padding-block, var(--gymx-size-00));
+  --_gymx-select-gap: var(--gymx-select-gap, var(--_gymx-select-padding-inline));
 
   display: flex;
   flex-wrap: nowrap;
   place-content: stretch;
-  font-family: var(--_select-font-family);
-  font-size: var(--_select-font-size);
-  font-weight: var(--_select-font-weight);
+  font-family: var(--_gymx-select-font-family);
+  font-size: var(--_gymx-select-font-size);
+  font-weight: var(--_gymx-select-font-weight);
 
-  color: var(--_select-color);
-  background: var(--_select-color-background);
-  border: var(--_select-border);
-  padding-inline: var(--_select-padding-inline);
-  padding-block: var(--_select-padding-block);
-  border-radius: var(--_select-radius);
+  color: var(--_gymx-select-color);
+  background: var(--_gymx-select-color-background);
+  border: var(--_gymx-select-border);
+  padding-inline: var(--_gymx-select-padding-inline);
+  padding-block: var(--_gymx-select-padding-block);
+  border-radius: var(--_gymx-select-radius);
 
   outline-offset: 1px;
 
   &__start:not(:empty) {
-    margin-inline-end: var(--_select-gap);
+    margin-inline-end: var(--_gymx-select-gap);
   }
 
   &__end:not(:empty) {
-    margin-inline-start: var(--_select-gap);
+    margin-inline-start: var(--_gymx-select-gap);
   }
 
   &__input-wrapper {
@@ -133,20 +133,20 @@ const [modelValue] = defineModel<SelectItem | SelectItem[]>();
 
   &--hover,
   &:hover {
-    color: var(--_select-color-hover);
-    background: var(--_select-color-background-hover);
-    border-color: var(--_select-color-border-hover);
+    color: var(--_gymx-select-color-hover);
+    background: var(--_gymx-select-color-background-hover);
+    border-color: var(--_gymx-select-color-border-hover);
   }
 
   &:has(#{$self}__input:focus-visible),
   &--focused {
-    outline: var(--_select-border);
+    outline: var(--_gymx-select-border);
   }
 
   &:is(&--disabled, [disabled]) {
-    color: var(--_select-color-disabled);
-    background: var(--_select-color-background-disabled);
-    border-color: var(--_select-color-border-disabled);
+    color: var(--_gymx-select-color-disabled);
+    background: var(--_gymx-select-color-background-disabled);
+    border-color: var(--_gymx-select-color-border-disabled);
   }
 }
 </style>
