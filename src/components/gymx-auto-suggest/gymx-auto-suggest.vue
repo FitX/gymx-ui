7c7667ac75e58b8068f3a93bb7601662389d9fc4
Michael Raguse <michael.raguse@vipion.de>
2025-05-12 11:02:14 +0200

    [GYMX-1172] fix error styles

--- src/components/gymx-auto-suggest/gymx-auto-suggest.vue
+++ src/components/gymx-auto-suggest/gymx-auto-suggest.vue
@@ -257,13 +257,13 @@ defineExpose({
   grid-template-rows: auto 1fr; */
 
   :deep(.text-field__additional) {
-    position: absolute;
+    // position: absolute;
     top: calc(100% + var(--gymx-text-field-gap, var(--gymx-size-00)));
   }
 
   &[data-expanded='true'] {
     :deep(.text-field__additional) {
-      // position: absolute;
+      position: absolute;
       opacity: 0;
       pointer-events: none;
     }
