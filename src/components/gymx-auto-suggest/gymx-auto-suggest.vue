d99471eb04ef75d1e5eb0610711598aeda059449
Michael Raguse <michael.raguse@vipion.de>
2025-05-27 15:22:55 +0200

    [#20] nope - rollback

--- src/components/gymx-auto-suggest/gymx-auto-suggest.vue
+++ src/components/gymx-auto-suggest/gymx-auto-suggest.vue
@@ -1,29 +1,11 @@
 <script setup lang="ts" generic="T extends Option">
-import { type InputHTMLAttributes, computed, ref, nextTick, watch } from 'vue';
+import { computed, ref, nextTick, watch } from 'vue';
 import { onClickOutside } from '@vueuse/core';
+import type { GymxAutoSuggestProps, Option } from './types';
 import { filter } from './utils';
 import { GymxTextField } from '@/components';
 
-export type Option = {
-  text: string;
-  value: string | number;
-  disabled?: boolean;
-}
-
-const props = withDefaults(defineProps<{
-  disabled?: boolean;
-  errorMessage?: string;
-  id?: string;
-  label: string;
-  name: string;
-  options: T[];
-  placeholder?: string;
-  readonly?: boolean;
-  required?: boolean;
-  noResultsText?: string;
-  filterFunction?: (options?: T[], text?: string) => T[];
-  inputAttributes?: InputHTMLAttributes;
-}>(), {
+const props = withDefaults(defineProps<GymxAutoSuggestProps<T>>(), {
   noResultsText: 'No results available',
   inputAttributes: () => ({
     type: 'search',
--- src/components/gymx-auto-suggest/types.ts
+++ src/components/gymx-auto-suggest/types.ts
@@ -1,2 +1,24 @@
-export type { Option } from './gymx-auto-suggest.vue';
-// export interface GymxAutoSuggestEmits {}
+import type { InputHTMLAttributes } from 'vue';
+
+export interface Option {
+  text: string;
+  value: string | number;
+  disabled?: boolean;
+}
+
+export interface GymxAutoSuggestProps <T extends Option>{
+  disabled?: boolean;
+  errorMessage?: string;
+  id?: string;
+  label: string;
+  name: string;
+  options: T[];
+  placeholder?: string;
+  readonly?: boolean;
+  required?: boolean;
+  noResultsText?: string;
+  filterFunction?: (options?: T[], text?: string) => T[];
+  inputAttributes?: InputHTMLAttributes;
+}
+
+export interface GymxAutoSuggestEmits {}
