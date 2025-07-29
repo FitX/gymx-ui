11ea622e85096104a3a2a62236fe94e17110ea08
Michael Raguse <michael.raguse@vipion.de>
2025-07-29 11:57:56 +0200

    [GYMX-603] handle single bool with v-model only

--- src/components/gymx-checkbox/gymx-checkbox.vue
+++ src/components/gymx-checkbox/gymx-checkbox.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup generic="T extends string | number | boolean | Array<string | number>">
-import type { GymxCheckboxProps } from './types.ts';
-import { getModifierClasses } from '@/utils/css-modifier.ts';
+import type { GymxCheckboxProps } from './types';
+import { getModifierClasses } from '@/utils/css-modifier';
 import { computed } from 'vue';
 
 defineOptions({
@@ -11,53 +11,70 @@ type CheckboxComponentProps = GymxCheckboxProps<T>;
 
 const props = withDefaults(defineProps<CheckboxComponentProps>(), {
   inputAttributes: () => ({}),
+  id: () => crypto.randomUUID(),
   state: undefined,
-})
+});
 
 const model = defineModel<T>({ required: true });
 
+const effectiveValue = computed(() => {
+  if (props.value !== undefined) {
+    return props.value;
+  }
+  if (!Array.isArray(model.value)) {
+    return true as T extends Array<infer U> ? U : T;
+  }
+  return undefined;
+});
+
 const isChecked = computed(() => {
   if (Array.isArray(model.value)) {
-    return (model.value as Array<string | number>).includes(props.value as string | number)
+    return (model.value as Array<string | number>).includes(
+      effectiveValue.value as string | number,
+    );
   }
-  return model.value === props.value
-})
+  return model.value === effectiveValue.value;
+});
 
 const handleChange = (event: Event) => {
-  const target = event.target as HTMLInputElement
+  const target = event.target as HTMLInputElement;
 
   if (Array.isArray(model.value)) {
-    // multi usw
-    const currentArray = model.value as Array<string | number>
-    const newValue = [...currentArray]
-    const valueToToggle = props.value as string | number
+    // multiple value
+    const currentArray = model.value as Array<string | number>;
+    const newValue = [...currentArray];
+    const valueToToggle = effectiveValue.value as string | number;
 
     if (target.checked) {
       if (!newValue.includes(valueToToggle)) {
-        newValue.push(valueToToggle)
+        newValue.push(valueToToggle);
       }
     } else {
-      const index = newValue.indexOf(valueToToggle)
+      const index = newValue.indexOf(valueToToggle);
       if (index > -1) {
-        newValue.splice(index, 1)
+        newValue.splice(index, 1);
       }
     }
-    model.value = newValue as T
+    model.value = newValue as T;
   } else {
-    // single
-    model.value = (target.checked ? props.value : false) as T
+    // singlle value
+    model.value = (target.checked ? effectiveValue.value : false) as T;
   }
-}
+};
 </script>
+
 <template>
-  <span class="checkbox" :class="[getModifierClasses('checkbox', props.state)]">
+  <span
+    class="checkbox"
+    :class="[getModifierClasses('checkbox', props.state)]">
     <input
       class="checkbox__input"
       type="checkbox"
       v-bind="props.inputAttributes"
+      :id="props.id"
       :checked="isChecked"
-      :value="props.value"
+      :value="effectiveValue"
       @change="handleChange" />
+    <slot></slot>
   </span>
 </template>
-<style lang="scss" scoped></style>
