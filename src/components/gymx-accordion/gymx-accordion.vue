5b384cac56df0ccfb44b45dab52ff82447d140c5
Michael Raguse <michael.raguse@vipion.de>
2025-08-27 14:09:49 +0200

    [GYMX-1482] handle open state

--- src/components/gymx-accordion/gymx-accordion.vue
+++ src/components/gymx-accordion/gymx-accordion.vue
@@ -2,21 +2,31 @@
 <script lang="ts" setup>
 import type { GymxAccordionProps } from './types';
 import { getModifierClasses } from '@/utils/css-modifier.ts';
+import { useTemplateRef, watch } from 'vue';
 
 const props = defineProps<GymxAccordionProps>();
+const open = defineModel<boolean>({ default: false });
+const element = useTemplateRef<HTMLDetailsElement>('element');
+
+const handleToggle = (e) => {
+  open.value = !!element.value?.open;
+};
 </script>
 <template>
   <details
+    ref="element"
     class="accordion"
     :class="[
       getModifierClasses('accordion', props.customIcon ? 'has-custom-icon' : undefined),
     ]"
-    :open="props.open"
+    :open="open"
+    @toggle="handleToggle"
     :name="props.name">
     <summary class="accordion__title">
-      <slot name="icon-start" v-bind="{ open: props.open }"></slot>
+      <slot name="icon-start" v-bind="{ props }"></slot>
       <slot name="title">{{ props.title }}</slot>
-      <slot name="icon-end" v-bind="{ open: props.open }"></slot>
+      <slot name="icon-end" :open="open"></slot>
+      {{ open }}
     </summary>
     <div class="accordion__content">
       <slot name="default">
